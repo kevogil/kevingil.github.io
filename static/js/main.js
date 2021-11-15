6fdac393f61f5036e5446581520e5025654bde79
Kevin Gil <kevingil23@gmail.com>
2021-11-15 09:07:07 -0800

    updating index

--- static/js/main.js
+++ static/js/main.js
@@ -97,7 +97,7 @@ window.onload = function() {
   }
   // INJECT CSS
   var css = document.createElement("style");
-  css.type = "./css/main.css";
+  css.type = "text/css";
   css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
   document.body.appendChild(css);
 };
