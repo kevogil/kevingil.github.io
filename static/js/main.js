96b88c75da8ad6aa5676aeed09069aa5ad4a9f03
Kevin Gil <kevingil23@gmail.com>
2021-11-15 12:25:27 -0800

    updating index

--- static/js/main.js
+++ static/js/main.js
@@ -52,7 +52,7 @@ window.onload = function() {
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
-  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #ffffff}";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #666}";
   document.body.appendChild(css);
 };
 // ----- Banner end -----
